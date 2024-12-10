7f0d2884c42fbb92d8906f8ea0b6699b61d086bd
gyf1214 <gyf1214@gmail.com>
2024-12-09 17:36:20 -0800

    Fix

--- src/main/java/org/shsts/tinactory/core/tech/TechManager.java
+++ src/main/java/org/shsts/tinactory/core/tech/TechManager.java
@@ -13,7 +13,6 @@ import net.minecraft.util.profiling.ProfilerFiller;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.scores.PlayerTeam;
 import net.minecraftforge.event.AddReloadListenerEvent;
-import net.minecraftforge.network.NetworkEvent;
 import org.shsts.tinactory.api.tech.IClientTechManager;
 import org.shsts.tinactory.api.tech.IServerTechManager;
 import org.shsts.tinactory.api.tech.ITeamProfile;
@@ -223,7 +222,7 @@ public class TechManager implements ITechManager {
             localTeam = null;
         }
 
-        private void handleTechInit(TechInitPacket p, NetworkEvent.Context ctx) {
+        private void handleTechInit(TechInitPacket p) {
             technologies.clear();
             var techs = p.getTechs();
             for (var tech : techs) {
@@ -233,7 +232,7 @@ public class TechManager implements ITechManager {
             LOGGER.debug("reload {} techs", technologies.size());
         }
 
-        private void handleTechUpdate(TechUpdatePacket p, NetworkEvent.Context ctx) {
+        private void handleTechUpdate(TechUpdatePacket p) {
             var team = getLocalTeam();
             if (team == null) {
                 return;
@@ -279,9 +278,9 @@ public class TechManager implements ITechManager {
         server = new Server();
         LOGGER.debug("create server tech manager {}", server);
         CHANNEL.registerClientPacket(TechInitPacket.class, TechInitPacket::new,
-            (p, ctx) -> client().handleTechInit(p, ctx));
+            (p, ctx) -> client().handleTechInit(p));
         CHANNEL.registerClientPacket(TechUpdatePacket.class, TechUpdatePacket::new,
-            (p, ctx) -> client().handleTechUpdate(p, ctx));
+            (p, ctx) -> client().handleTechUpdate(p));
     }
 
     public static void initClient() {
