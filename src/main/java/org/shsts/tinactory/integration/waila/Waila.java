db4d3b3ae5def129f40c3053ce8d653189d0a717
gyf1214 <gyf1214@gmail.com>
2025-11-07 13:49:07 -0800

    Waila support

---
+++ src/main/java/org/shsts/tinactory/integration/waila/Waila.java
@@ -0,0 +1,12 @@
+package org.shsts.tinactory.integration.waila;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+import mcp.mobius.waila.api.IWailaPlugin;
+import mcp.mobius.waila.api.WailaPlugin;
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+@WailaPlugin
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+public class Waila implements IWailaPlugin {
+}
