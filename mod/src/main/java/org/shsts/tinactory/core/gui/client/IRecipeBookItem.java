a59e0db1698c7a6ce001dde560cd9c06fb5cbb14
gyf1214 <gyf1214@gmail.com>
2026-04-24 15:06:00 -0700

    Remove IRenderable

--- mod/src/main/java/org/shsts/tinactory/core/gui/client/IRecipeBookItem.java
+++ mod/src/main/java/org/shsts/tinactory/core/gui/client/IRecipeBookItem.java
@@ -1,5 +1,6 @@
 package org.shsts.tinactory.core.gui.client;
 
+import com.mojang.blaze3d.vertex.PoseStack;
 import javax.annotation.ParametersAreNonnullByDefault;
 import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.network.chat.Component;
@@ -7,6 +8,7 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import org.shsts.tinactory.api.recipe.IProcessingObject;
 import org.shsts.tinactory.core.gui.Layout;
+import org.shsts.tinactory.core.gui.Rect;
 import org.shsts.tinycorelib.api.core.ILoc;
 
 import java.util.List;
@@ -16,10 +18,12 @@ import java.util.function.BiConsumer;
 @OnlyIn(Dist.CLIENT)
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
-public interface IRecipeBookItem extends ILoc, IRectRenderable {
+public interface IRecipeBookItem extends ILoc {
     boolean isMarker();
 
     void select(Layout layout, BiConsumer<Layout.SlotInfo, IProcessingObject> ingredientCons);
 
     Optional<List<Component>> buttonToolTip();
+
+    void render(PoseStack poseStack, Rect rect, int z);
 }
