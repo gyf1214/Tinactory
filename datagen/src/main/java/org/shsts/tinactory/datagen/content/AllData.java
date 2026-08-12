8ca54494e027a2ca225890efe9ea002500c9d924
gyf1214 <gyf1214@gmail.com>
2026-08-12 14:27:38 -0700

    convert player start to nbt structure

--- datagen/src/main/java/org/shsts/tinactory/datagen/content/AllData.java
+++ datagen/src/main/java/org/shsts/tinactory/datagen/content/AllData.java
@@ -2,14 +2,26 @@ package org.shsts.tinactory.datagen.content;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 import net.minecraft.MethodsReturnNonnullByDefault;
+import net.minecraft.data.DataProvider;
+import net.minecraft.data.structures.SnbtToNbt;
+import net.neoforged.neoforge.data.event.GatherDataEvent;
+import org.shsts.tinycorelib.datagen.api.IDataGen;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.shsts.tinactory.datagen.TinactoryDatagen.DATA_GEN;
 
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
 public final class AllData {
     private static final List<Runnable> DELAYED_CALLBACKS = new ArrayList<>();
+
+    private static DataProvider snbtProvider(IDataGen dataGen, GatherDataEvent event) {
+        var modFile = event.getModContainer().getModInfo().getOwningFile().getFile();
+        var folder = modFile.findResource("snbt");
+        return new SnbtToNbt(event.getGenerator().getPackOutput(), List.of(folder));
+    }
 
     public static void initDelayed(Runnable cb) {
         DELAYED_CALLBACKS.add(cb);
@@ -18,6 +30,8 @@ public final class AllData {
     public static void init() {
         Models.init();
         AllDataKt.INSTANCE.init();
+
+        DATA_GEN.addProvider(AllData::snbtProvider);
 
         for (var cb : DELAYED_CALLBACKS) {
             cb.run();
--- mod/src/main/java/org/shsts/tinactory/content/worldgen/PlayerStartFeature.java
+++ mod/src/main/java/org/shsts/tinactory/content/worldgen/PlayerStartFeature.java
@@ -2,18 +2,20 @@ package org.shsts.tinactory.content.worldgen;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 import net.minecraft.MethodsReturnNonnullByDefault;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 import net.minecraft.world.level.levelgen.feature.configurations.NoneFeatureConfiguration;
-import org.shsts.tinactory.AllItems;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructurePlaceSettings;
+
+import static org.shsts.tinactory.core.util.LocHelper.modLoc;
 
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
 public class PlayerStartFeature extends Feature<NoneFeatureConfiguration> {
+    private static final ResourceLocation STRUCTURE = modLoc("player_start");
+
     public PlayerStartFeature() {
         super(NoneFeatureConfiguration.CODEC);
     }
@@ -22,25 +24,9 @@ public class PlayerStartFeature extends Feature<NoneFeatureConfiguration> {
     public boolean place(FeaturePlaceContext<NoneFeatureConfiguration> ctx) {
         var world = ctx.level();
         var pos = ctx.origin();
-        for (var i = -1; i < 2; i++) {
-            for (var j = -1; j < 2; j++) {
-                world.setBlock(pos.offset(i, 0, j), Blocks.OBSIDIAN.defaultBlockState(), 3);
-            }
-        }
-        var chestPos = pos.offset(0, 1, 1);
-        world.setBlock(chestPos, Blocks.CHEST.defaultBlockState(), 3);
-        world.setBlock(chestPos.east(), Blocks.TORCH.defaultBlockState(), 3);
-        world.setBlock(chestPos.west(), Blocks.TORCH.defaultBlockState(), 3);
-        world.getBlockEntity(chestPos, BlockEntityType.CHEST).ifPresent(chest -> {
-            chest.setItem(0, new ItemStack(Blocks.GRASS_BLOCK, 64));
-            chest.setItem(1, new ItemStack(Blocks.DIRT, 64));
-            chest.setItem(2, new ItemStack(Items.OAK_SAPLING, 16));
-            chest.setItem(3, new ItemStack(AllItems.RUBBER_SAPLING.get(), 16));
-            chest.setItem(4, new ItemStack(Items.BONE_MEAL, 16));
-            chest.setItem(5, new ItemStack(Items.WATER_BUCKET));
-            chest.setItem(6, new ItemStack(Items.WATER_BUCKET));
-            chest.setItem(7, new ItemStack(Items.TORCH, 64));
-        });
+        var structure = world.getLevel().getStructureManager().getOrCreate(STRUCTURE);
+        structure.placeInWorld(world, pos, pos, new StructurePlaceSettings(),
+            ctx.random(), Block.UPDATE_CLIENTS);
         return true;
     }
 }
