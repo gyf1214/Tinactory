759c2032abeaaf8f89d2aaaa6b5edb241850fbf5
gyf1214 <gyf1214@gmail.com>
2026-08-23 08:47:16 -0700

    Fix format

--- mod/src/main/java/org/shsts/tinactory/content/logistics/MEStorageCellAliases.java
+++ mod/src/main/java/org/shsts/tinactory/content/logistics/MEStorageCellAliases.java
@@ -3,31 +3,33 @@ package org.shsts.tinactory.content.logistics;
 import javax.annotation.ParametersAreNonnullByDefault;
 import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceLocation;
 import net.neoforged.neoforge.registries.RegisterEvent;
+
+import static org.shsts.tinactory.core.util.LocHelper.modLoc;
 
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
 public final class MEStorageCellAliases {
-    private static final String[] FAMILIES = {"component/storage_component", "logistics/item_storage_cell",
-        "logistics/fluid_storage_cell", "logistics/pattern_cell"};
+    private static final String[] FAMILIES = {
+        "component/storage_component",
+        "logistics/item_storage_cell",
+        "logistics/fluid_storage_cell",
+        "logistics/pattern_cell"
+    };
     private static final String[] LEGACY_NAMES = {"1m", "4m", "16m", "64m"};
 
     private MEStorageCellAliases() {}
 
     public static void registerAliases(RegisterEvent event) {
-        if (event.getRegistryKey() != Registries.ITEM) {
+        var registry = event.getRegistry(Registries.ITEM);
+        if (registry == null) {
             return;
         }
-        var registry = event.getRegistry(Registries.ITEM);
         for (var family : FAMILIES) {
             for (var index = 0; index < LEGACY_NAMES.length; index++) {
-                registry.addAlias(id(family + "/" + LEGACY_NAMES[index]), id(family + "/tier_" + (index + 1)));
+                registry.addAlias(modLoc(family + "/" + LEGACY_NAMES[index]),
+                    modLoc(family + "/tier_" + (index + 1)));
             }
         }
-    }
-
-    private static ResourceLocation id(String path) {
-        return ResourceLocation.fromNamespaceAndPath("tinactory", path);
     }
 }
